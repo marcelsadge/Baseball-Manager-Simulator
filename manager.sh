5a605df03ae55c6edf1b42e38b8dcc7a818fbb0c
Junhao Zhang <zjunhao@eniac.seas.upenn.edu>
2023-04-23 22:01:04 -0400

    add initial game prompt

--- manager.sh
+++ manager.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-function start_game() {
-
+function start_manager() {
+	printf "\nWelcome to Baseball Manager 1910! Select an option to begin your experience.\n"
+	printf "\t[1] Start Game\n\t[2] Stats\n\t[3] Search\n\t[<Enter> or CTRL+D] Quit\n"
+	read option
+	while [ ! -z "$option" ]; do
+		case "$option" in 
+			1)
+				start_game
+				;;
+			2)
+				stats
+				;;
+			3)
+				search
+				;;
+			*)
+				printf "Invalid option.\n"
+		esac
+		printf "\nWelcome to Baseball Manager 1910! Select an option to begin your experience.\n"
+		printf "\t[1] Start Game\n\t[2] Stats\n\t[3] Search\n\t[<Enter> or CTRL+D] Quit\n"
+		read option
+	done
+	printf "Closing game...\n"
+	exit 0
 }
 
-start_game
+# Start the game
+
+start_manager
